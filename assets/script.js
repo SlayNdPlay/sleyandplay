47d3038c59475373e0824910068e68cce85526b2
SlayNdPlay <137083442+SlayNdPlay@users.noreply.github.com>
2023-06-19 17:45:39 +0200

    Delete assets directory

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-// Skrypty JavaScript
-
-// Podążanie menu głównego za scrollem
-window.addEventListener('scroll', function() {
-  const nav = document.querySelector('nav');
-  nav.classList.toggle('sticky', window.scrollY > 0);
-});
